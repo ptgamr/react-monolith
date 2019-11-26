fd8429be398c91e2222f262ad9283725ff796444
Anh Trinh <anh.trinhtrung@gmail.com>
2019-11-27 11:37:42 +1300

    Metro: make use of blacklistRE to teach metro not to import React from other package
    We want to blacklist React from other packages to enter the Module
    Resolution tree.
    
    But if the dependency has `import React from 'react'`, it will no longer
    being able to resolve React anymore.
    
    That is where `extraNodeModules` path come in, here we teach Metro where
    to find `react` when it is imported from file outside of the current tree.

--- MobileApp/metro.config.js
+++ MobileApp/metro.config.js
@@ -1,6 +1,11 @@
 const path = require('path');
+const blacklist = require('metro-config/src/defaults/blacklist');
 
 const modules = ['monolith-shared'];
+const blacklisted = arrayFlatMap(modules, module => [
+  new RegExp(`${module}/node_modules/react-native/.*`),
+  new RegExp(`${module}/node_modules/react/.*`),
+]);
 
 module.exports = {
   transformer: {
@@ -17,9 +22,18 @@ module.exports = {
     // the ancestor directories of those files and doesn't look in our
     // own node_modules.
     extraNodeModules: {
+      react: path.resolve(__dirname, 'node_modules/react'),
+      'react-native': path.resolve(__dirname, 'node_modules/react-native'),
       '@babel/runtime': path.resolve(__dirname, 'node_modules/@babel/runtime'),
     },
+    // these path won't enter Module Resolution tree
+    blacklistRE: blacklist(blacklisted),
   },
   // this teaches Metro to look outside of the MobileApp tree
   watchFolders: modules.map(moduleName => path.join(__dirname, `../${moduleName}`))
 };
+
+// Backport of Array.flatMap.
+function arrayFlatMap(a, f) {
+  return [].concat(...a.map(f));
+}
