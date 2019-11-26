1adc6298eba062a6ce4706b3735a01c2441e2c08
Anh Trinh <anh.trinhtrung@gmail.com>
2019-11-27 11:30:16 +1300

    Update Metro config to extends `watchFolders` & `resolver.extraNodeModules`, that it can load modules outside of the MobileApp tree
    The app works if we only import non-react dependencies.
    
    ```typescript
    import {getPlural} from '../monolith-shared'
    
    // then use getPlural in the code
    ```
    
    As soon as we import a hook from shared module, the red screen appear on
    mobile with the error:
    
    ```
    ExceptionsManager.js:44 Error: Invalid hook call. Hooks can only be called inside of the body of a function component. This could happen for one of the following reasons:
    1. You might have mismatching versions of React and the renderer (such as React DOM)
    2. You might be breaking the Rules of Hooks
    3. You might have more than one copy of React in the same app
    ```
    
    The most likely reason is that we have multiple versions of React resolved inside the mobile
    project

--- MobileApp/metro.config.js
+++ MobileApp/metro.config.js
@@ -1,9 +1,6 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const path = require('path');
+
+const modules = ['monolith-shared'];
 
 module.exports = {
   transformer: {
@@ -14,4 +11,15 @@ module.exports = {
       },
     }),
   },
+  resolver: {
+    // These are to help Metro find modules imported from files found
+    // outside our tree.  Without it, Metro tries to resolve them from
+    // the ancestor directories of those files and doesn't look in our
+    // own node_modules.
+    extraNodeModules: {
+      '@babel/runtime': path.resolve(__dirname, 'node_modules/@babel/runtime'),
+    },
+  },
+  // this teaches Metro to look outside of the MobileApp tree
+  watchFolders: modules.map(moduleName => path.join(__dirname, `../${moduleName}`))
 };
